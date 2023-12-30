58a3e7b7bef8e084af2dd0eb26580dcad44dc542
Fabian-Faraz Farid <63512528+farazfarid@users.noreply.github.com>
2023-12-31 00:49:31 +0100

    fixing design inconsistency

--- studymate/src/components/ai/Hub.jsx
+++ studymate/src/components/ai/Hub.jsx
@@ -5,7 +5,7 @@ import {
   MessagesSquare,
   Shuffle,
 } from "lucide-react";
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Hub() {
   return (
@@ -16,46 +16,46 @@ function Hub() {
         <p className="sectionSubText">Select an AI tool to get started.</p>
       </div>
       <div class="nav-container">
-        <a
-          href="/ai-hub/content-writer"
+        <Link
+          to="/ai-hub/content-writer"
           class="nav-item bg-white/10 rounded-md"
         >
           <p class="flex text-xl font-semibold">
             <FileText className="mx-2" />
             AI Content Writer
           </p>
-        </a>
+        </Link>
 
-        <a href="/ai-hub/summariser" class="nav-item bg-white/10 rounded-md">
+        <Link to="/ai-hub/summariser" class="nav-item bg-white/10 rounded-md">
           <p class="flex text-xl font-semibold">
             <BookOpenText className="mx-2" />
             AI Summariser
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="/ai-hub/chat"
+        <Link
+          to="/ai-hub/chat"
           class="nav-item bg-white/10 rounded-md col-span-2"
         >
           <p class="flex text-xl font-semibold">
             <MessagesSquare className="mx-2" />
             AI Chat
           </p>
-        </a>
+        </Link>
 
-        <a href="/ai-hub/paraphraser" class="nav-item bg-white/10 rounded-md">
+        <Link to="/ai-hub/paraphraser" class="nav-item bg-white/10 rounded-md">
           <p class="flex text-xl font-semibold">
             <Shuffle className="mx-2" />
             AI Paraphraser
           </p>
-        </a>
+        </Link>
 
-        <a href="/ai-hub/translator" class="nav-item bg-white/10 rounded-md">
+        <Link to="/ai-hub/translator" class="nav-item bg-white/10 rounded-md">
           <p class="flex text-xl font-semibold">
             <Globe2 className="mx-2" />
             AI Translator
           </p>
-        </a>
+        </Link>
       </div>
     </div>
   );
