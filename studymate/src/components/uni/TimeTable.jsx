1c5567a5be6367ae55264cd18f2e36ae1f1ea85a
Fabian-Faraz Farid <63512528+farazfarid@users.noreply.github.com>
2023-12-30 02:37:53 +0100

    adding timetable

--- studymate/src/components/uni/TimeTable.jsx
+++ studymate/src/components/uni/TimeTable.jsx
@@ -1,49 +1,7 @@
-// src/components/Timetable.js
-import React, { useState } from "react";
-import TimetableHeader from "./TimetableHeader";
-import TimetableBody from "./TimetableBody";
-import AddSubjectModal from "./AddSubjectModal";
+import React from "react";
 
-const Timetable = () => {
-  const [subjects, setSubjects] = useState([]);
-  const [selectedSubject, setSelectedSubject] = useState(null);
-  const [isAddSubjectModalOpen, setIsAddSubjectModalOpen] = useState(false);
-
-  const addSubject = (subject) => {
-    setSubjects([...subjects, subject]);
-  };
-
-  const editSubject = (editedSubject) => {
-    const updatedSubjects = subjects.map((s) =>
-      s.id === editedSubject.id ? editedSubject : s
-    );
-    setSubjects(updatedSubjects);
-    setSelectedSubject(null);
-  };
-
-  const deleteSubject = (id) => {
-    const updatedSubjects = subjects.filter((s) => s.id !== id);
-    setSubjects(updatedSubjects);
-    setSelectedSubject(null);
-  };
-
-  return (
-    <div className="container mx-auto mt-5">
-      <TimetableHeader onAddSubject={() => setIsAddSubjectModalOpen(true)} />
-      <TimetableBody
-        subjects={subjects}
-        onEditSubject={editSubject}
-        onDeleteSubject={deleteSubject}
-        onSelectSubject={setSelectedSubject}
-        selectedSubject={selectedSubject}
-      />
-      <AddSubjectModal
-        isOpen={isAddSubjectModalOpen}
-        onRequestClose={() => setIsAddSubjectModalOpen(false)}
-        onAddSubject={addSubject}
-      />
-    </div>
-  );
-};
+function Timetable() {
+  return <div>Timetable</div>;
+}
 
 export default Timetable;
